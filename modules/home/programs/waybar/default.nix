fbcee13f65233e5cd874225113f4ea76d2b3a136
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-02-20 16:02:05 +0300

    update waybar configuration

--- modules/home/programs/waybar/default.nix
+++ modules/home/programs/waybar/default.nix
@@ -14,7 +14,7 @@
         spacing = 4;
         modules-left = ["hyprland/workspaces" "hyprland/window"];
         modules-center = ["clock"];
-        modules-right = ["pulseaudio" "network" "battery" "tray"];
+        modules-right = ["pulseaudio" "cpu" "memory" "tray"];
 
         "hyprland/workspaces" = {
           disable-scroll = true;
@@ -22,44 +22,103 @@
           format = "{icon}";
         };
         "clock" = {
-          format = "{:%H:%M - %a, %d %b}";
+          format = " {:%H:%M   %a, %d %b}";
           tooltip-format = "<big>{:%Y %B}</big>\n<tt>{calendar}</tt>";
         };
         "pulseaudio" = {
-          format = "{volume}% 🔊";
-          format-muted = "Muted 🔇";
+          format = "{icon} {volume}%";
+          format-muted = "󰝟 Muted";
+          format-icons = {
+            headphone = "";
+
+            default = ["" "" ""];
+          };
           on-click = "pavucontrol";
+        };
+        "cpu" = {
+          format = " {usage}%";
+          tooltip = false;
+        };
+        "memory" = {
+          format = " {used}/{total}GB";
+          tooltip = false;
+        };
+        "tray" = {
+          icon-size = 16;
         };
       };
     };
     style = ''
+
+      @define-color base            #191724;
+      @define-color surface         #1f1d2e;
+      @define-color overlay         #26233a;
+
+      @define-color muted           #6e6a86;
+      @define-color subtle          #908caa;
+      @define-color text            #e0def4;
+
+      @define-color love            #eb6f92;
+      @define-color gold            #f6c177;
+      @define-color rose            #ebbcba;
+      @define-color pine            #31748f;
+      @define-color foam            #9ccfd8;
+      @define-color iris            #c4a7e7;
+
+      @define-color highlightLow    #21202e;
+      @define-color highlightMed    #403d52;
+      @define-color highlightHigh   #524f67;
       * {
-          font-family: "JetBrainsMono Nerd Font", "Font Awesome 6 Free", sans-serif;
+          font-family: "JetBrainsMono Nerd Font";
           font-size: 13px;
           font-weight: bold;
       }
       window#waybar {
-          background-color: #191724; /* Base */
-          color: #e0def4; /* Text */
-          border-bottom: 2px solid #ebbcba; /* Rose Accent */
+          background-color: rgba(25, 23, 36, 0.85); /* base but 0.85 */
+          color: @text;
+          border-bottom: 2px solid @rose; 
+          transition-property: background-color;
+          transition-duration: .5s;
       }
       #workspaces button {
           padding: 0 10px;
           background-color: transparent;
-          color: #908caa; /* Subtle */
+          color: @subtle;
+          border-bottom: 3px solid transparent;
+          transition: all 0.3s ease;
       }
       #workspaces button.active {
-          background-color: #ebbcba; /* Rose */
-          color: #191724; /* Base */
+          background-color: rgba(235, 188, 186, 0.1); /* rose but low opacity */
+          color: @rose;
+          border-bottom: 3px solid @rose;
           border-radius: 0;
       }
-      #clock, #pulseaudio, #network, #battery, #tray {
+      #workspaces button:hover {
+          background-color: rgba(235, 188, 186, 0.2);
+          color: @text;
+          box-shadow: inherit;
+          text-shadow: inherit;
+      }
+      #clock, #pulseaudio, #cpu, #memory, #network, #battery, #tray {
           padding: 0 12px;
-          color: #e0def4; /* Text */
+          margin: 4px 0;
+          color: @text;
+      }
+      #pulseaudio {
+          color: @rose;
+      }
+      #cpu {
+          color: @pine;
+      }
+      #memory {
+          color: @foam;
       }
       #window {
           margin-left: 20px;
-          color: #c4a7e7; /* Iris */
+          color: @iris;
+      }
+      #tray {
+          margin-right: 10px;
       }
     '';
   };
