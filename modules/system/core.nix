4b0f242ac381ef382b33ac750441068954f7dfb4
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-02-20 23:21:58 +0300

    add gta 5 battleye blocks to hosts

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -6,6 +6,9 @@
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  networking.hosts = {
+    "0.0.0.0" = ["paradise-s1.battleye.com" "test-s1.battleye.com" "paradiseenhanced-s1.battleye.com"];
+  };
 
   time.timeZone = "Asia/Amman";
 
