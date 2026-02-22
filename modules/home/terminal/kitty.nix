26d5a8d0e473bd2a3242a5b78e8651ef3e2dcf69
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-02-23 01:06:14 +0300

    keybinds for kitty

--- modules/home/terminal/kitty.nix
+++ modules/home/terminal/kitty.nix
@@ -11,6 +11,22 @@
       font_size = 12;
       background_opacity = "0.95";
       window_padding_width = 10;
+      confirm_os_window_close = 0;
+      enableFishIntegration = true;
+    };
+    keybindings = {
+      "ctrl+c" = "copy_or_interrupt";
+      "page_up" = "scroll_page_up";
+      "page_down" = "scroll_page_down";
+      "ctrl+plus" = "change_font_size all +1";
+      "ctrl+equal" = "change_font_size all +1";
+      "ctrl+kp_add" = "change_font_size all +1";
+      "ctrl+minus" = "change_font_size all -1";
+      "ctrl+underscore" = "change_font_size all -1";
+      "ctrl+kp_subtract" = "change_font_size all -1";
+      "ctrl+0" = "change_font_size all 0";
+      "ctrl+kp_0" = "change_font_size all 0";
+      "ctrl+l" = "clear_terminal scroll active";
     };
   };
 }
