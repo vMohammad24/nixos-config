f9df52bc7a212386bac5a6a317788d2e9cf91009
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-03-01 01:24:51 +0300

    rely on uwsm more

--- modules/home/windowManager/hyprland/env.nix
+++ modules/home/windowManager/hyprland/env.nix
@@ -6,9 +6,6 @@
   wayland.windowManager.hyprland.settings.env = [
     "HYPRCURSOR_THEME,rose-pine-hyprcursor"
     "HYPRCURSOR_SIZE,24"
-    "XDG_CURRENT_DESKTOP,Hyprland"
-    "XDG_SESSION_TYPE,wayland"
-    "XDG_SESSION_DESKTOP,Hyprland"
     "ADW_DISABLE_PORTAL,1"
   ];
 }
--- modules/home/windowManager/hyprland/startup.nix
+++ modules/home/windowManager/hyprland/startup.nix
@@ -4,11 +4,9 @@
   ...
 }: {
   wayland.windowManager.hyprland.settings.exec-once = [
-    "${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent"
-    "udiskie --autostart --tray --notify"
-    "hyprpaper"
-    "mako"
-    "wl-paste --watch cliphist store"
+    "uwsm app -- ${pkgs.hyprpolkitagent}/libexec/hyprpolkitagent"
+    "uwsm app -- udiskie --autostart --tray --notify"
+    "uwsm app -- wl-paste --watch cliphist store"
     "gsettings set org.gnome.desktop.interface gtk-theme 'rose-pine'"
     "gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'"
     "gsettings set org.gnome.desktop.interface icon-theme 'rose-pine'"
