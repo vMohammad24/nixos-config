8ed8bc4bf0ae667938e4852515bc8f0ae8d6455b
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-03-14 03:14:25 +0300

    mpv

--- modules/home/core.nix
+++ modules/home/core.nix
@@ -1,4 +1,5 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   home.username = "vmohammad";
   home.homeDirectory = "/home/vmohammad";
   home.stateVersion = "25.11";
@@ -8,17 +9,17 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "text/*" = ["firefox.desktop"];
-      "application/json" = ["dev.zed.Zed.desktop"];
-      "x-scheme-handler/http" = ["firefox.desktop"];
-      "x-scheme-handler/https" = ["firefox.desktop"];
-      "application/pdf" = ["firefox.desktop"];
-      "inode/directory" = ["org.gnome.Nautilus.desktop"];
-      "image/jpeg" = ["qview.desktop"];
-      "image/png" = ["qview.desktop"];
-      "image/webp" = ["qview.desktop"];
-      "image/gif" = ["qview.desktop"];
-      "image/svg+xml" = ["qview.desktop"];
+      "text/*" = [ "firefox.desktop" ];
+      "application/json" = [ "dev.zed.Zed.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "application/pdf" = [ "firefox.desktop" ];
+      "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+      "image/jpeg" = [ "qview.desktop" ];
+      "image/png" = [ "qview.desktop" ];
+      "image/webp" = [ "qview.desktop" ];
+      "image/gif" = [ "qview.desktop" ];
+      "image/svg+xml" = [ "qview.desktop" ];
     };
   };
 
@@ -38,6 +39,7 @@
     bs-manager
     teamspeak6-client
     qview
+    mpv
     bruno
 
     # utils/essentials
