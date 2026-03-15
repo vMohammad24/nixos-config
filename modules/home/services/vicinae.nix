2042dadbd1cf68fc24dbfddb017e4799e3270cf0
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-03-16 01:56:43 +0300

    vicinae config 👍

--- modules/home/services/vicinae.nix
+++ modules/home/services/vicinae.nix
@@ -1,5 +1,74 @@
-{...}: {
-  services.vicinae = {
+{ inputs, pkgs, ... }:
+{
+  programs.vicinae = {
     enable = true;
+    package = inputs.vicinae.packages.${pkgs.system}.default;
+    settings = {
+      close_on_focus_loss = false;
+      activate_on_single_click = true;
+      favicon_service = "google";
+      general = {
+        telemetry = false;
+      };
+      providers = {
+        applications = {
+          preferences = {
+            defaultAction = "focus";
+            launchPrefix = "uwsm app --";
+          };
+        };
+        clipboard = {
+          entrypoints = {
+            history = {
+              preferences = {
+                defaultAction = "copy";
+              };
+            };
+          };
+        };
+        core = {
+          entrypoints = {
+            sponsor = {
+              enabled = false;
+            };
+          };
+        };
+        developer = {
+          enabled = false;
+          entrypoints = {
+            create = {
+              enabled = false;
+            };
+          };
+        };
+        files = {
+          preferences = {
+            autoIndexing = true;
+            excludedPaths = "/nix/store";
+            paths = "~/";
+            watcherPaths = "";
+          };
+        };
+        font = {
+          enabled = false;
+        };
+        power = {
+          entrypoints = {
+            hibernate = {
+              enabled = false;
+            };
+            suspend = {
+              enabled = false;
+            };
+          };
+        };
+        system = {
+          enabled = false;
+        };
+      };
+    };
+    systemd = {
+      enable = false;
+    };
   };
 }
