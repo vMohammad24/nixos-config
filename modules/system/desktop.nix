20f6d118aad68bbbf795ff2a1176242b4d9a4b31
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-04-08 12:39:42 +0300

    add an arabic font

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -14,14 +14,41 @@
     config.common.default = ["*"];
   };
 
-  fonts.packages = with pkgs; [
-    jetbrains-mono
-    nerd-fonts.jetbrains-mono
-    noto-fonts
-    noto-fonts-lgc-plus
-    ibm-plex
-    amiri
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      jetbrains-mono
+      nerd-fonts.jetbrains-mono
+      noto-fonts
+      noto-fonts-lgc-plus
+      ibm-plex
+      vazir-fonts
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        monospace = [
+          "JetBrainsMono Nerd Font"
+          "JetBrains Mono"
+          "IBM Plex Mono"
+          "Vazirmatn"
+          "Noto Sans Mono"
+        ];
+
+        sansSerif = [
+          "IBM Plex Sans"
+          "Vazirmatn"
+          "Noto Sans Arabic"
+        ];
+
+        serif = [
+          "IBM Plex Serif"
+          "Noto Kufi Arabic"
+          "Vazirmatn"
+        ];
+      };
+    };
+  };
 
   environment.sessionVariables = {
     GIO_EXTRA_MODULES = ["${pkgs.glib-networking}/lib/gio/modules"];
