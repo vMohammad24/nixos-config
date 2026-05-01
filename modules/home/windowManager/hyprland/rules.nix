f5009227c9ec06e74878e60b58de7e56cb2ab7d1
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-05-01 12:34:23 +0300

    move firefox to new window rule syntax, add vicinae blur.

--- modules/home/windowManager/hyprland/rules.nix
+++ modules/home/windowManager/hyprland/rules.nix
@@ -1,7 +1,22 @@
 {...}: {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
-      "match:class ^(firefox)$, match:title ^(Picture-in-Picture)$, float on, pin on, suppress_event fullscreen maximize"
+      {
+        name = "firefox-pip";
+        float = true;
+        pin = true;
+        suppress_event = ["fullscreen" "maximize"];
+        "match:class" = "^(firefox)$";
+        "match:title" = "^(Picture-in-Picture)$";
+      }
+    ];
+    layerrule = [
+      {
+        name = "vicinae-blur";
+        blur = true;
+        ignore_alpha = 0;
+        "match:namespace" = "vicinae";
+      }
     ];
   };
 }
