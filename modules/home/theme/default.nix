e2c5122fddacc6510bfd0842fc491059c382a48f
vMohammad24 <62218284+vMohammad24@users.noreply.github.com>
2026-05-19 00:05:37 +0300

    remove manual gtk theming

--- modules/home/theme/default.nix
+++ modules/home/theme/default.nix
@@ -1,49 +1,14 @@
-{
-  pkgs,
-  lib,
-  osConfig,
-  ...
-}: {
+{pkgs, ...}: {
   stylix.targets = {
     hyprlock.enable = false;
     waybar.enable = false;
-    gtk.enable = false;
-    qt.enable = false;
-  };
-
-  qt = lib.mkIf (osConfig.myConfig.desktop == "hyprland") {
-    enable = true;
-    platformTheme.name = "gtk";
-    style.name = "gtk2";
   };
 
   gtk = {
     enable = true;
-    gtk4.theme = null;
-    theme = {
-      name = "rose-pine";
-      package = pkgs.rose-pine-gtk-theme;
-    };
     iconTheme = {
       name = "rose-pine";
       package = pkgs.rose-pine-icon-theme;
     };
-  };
-
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      gtk-theme = "rose-pine";
-    };
-  };
-
-  xdg.configFile = {
-    "gtk-4.0/assets".source = "${pkgs.rose-pine-gtk-theme}/share/themes/rose-pine/gtk-4.0/assets";
-    "gtk-4.0/gtk.css".source = "${pkgs.rose-pine-gtk-theme}/share/themes/rose-pine/gtk-4.0/gtk.css";
-    "gtk-4.0/gtk-dark.css".source = "${pkgs.rose-pine-gtk-theme}/share/themes/rose-pine/gtk-4.0/gtk-dark.css";
-    "gtk-4.0/settings.ini".text = ''
-      [Settings]
-      gtk-application-prefer-dark-theme=1
-    '';
   };
 }
