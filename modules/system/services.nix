f5fdae58d97c0cd01a057a8a7571d42f18ccf9c3
vMohammad24 <git@vmohammad.dev>
2026-06-25 20:33:06 +0300

    waydroid: enable on desktop

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -14,6 +14,7 @@ in {
     };
   };
   virtualisation.libvirtd.enable = isDesktop;
+  virtualisation.waydroid.enable = isDesktop;
   virtualisation.docker.rootless = lib.mkIf isDesktop {
     enable = true;
     setSocketVariable = true;
