aee29e6ff28cbbf33a2b4aa443b25b158f3c1255
vMohammad24 <git@vmohammad.dev>
2026-06-26 14:06:35 +0300

    secrets: limit *rr secrets to when rr is enabled

--- hosts/server/secrets.nix
+++ hosts/server/secrets.nix
@@ -1,56 +1,64 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   age = {
     identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
 
-    secrets = {
-      cloudflared = {
-        file = ../../secrets/cloudflared.age;
-        mode = "0400";
-      };
-      tss = {
-        file = ../../secrets/tss.age;
-        mode = "0400";
-      };
-      heliopolis-runner-token = {
-        file = ../../secrets/heliopolis-runner-token.age;
-        mode = "0400";
-        owner = "forgejo-runner";
-        group = "forgejo-runner";
-      };
-      mullvad-wg = {
-        file = ../../secrets/mullvad-wg.age;
-        mode = "0400";
-      };
-      prowlarr-milkie-apikey = {
-        file = ../../secrets/prowlarr-milkie-apikey.age;
-        mode = "0400";
-        owner = "prowlarr";
-        group = "prowlarr";
-      };
-      prowlarr-torrentleech-password = {
-        file = ../../secrets/prowlarr-torrentleech-password.age;
-        mode = "0400";
-        owner = "prowlarr";
-        group = "prowlarr";
-      };
-      prowlarr-torrentleech-2fa = {
-        file = ../../secrets/prowlarr-torrentleech-2fa.age;
-        mode = "0400";
-        owner = "prowlarr";
-        group = "prowlarr";
-      };
-      prowlarr-seedpool-apikey = {
-        file = ../../secrets/prowlarr-seedpool-apikey.age;
-        mode = "0400";
-        owner = "prowlarr";
-        group = "prowlarr";
-      };
-      grafana-secret-key = {
-        file = ../../secrets/grafana-secret-key.age;
-        mode = "0400";
-        owner = "grafana";
-        group = "grafana";
-      };
-    };
+    secrets = lib.mkMerge [
+      {
+        cloudflared = {
+          file = ../../secrets/cloudflared.age;
+          mode = "0400";
+        };
+        tss = {
+          file = ../../secrets/tss.age;
+          mode = "0400";
+        };
+        heliopolis-runner-token = {
+          file = ../../secrets/heliopolis-runner-token.age;
+          mode = "0400";
+          owner = "forgejo-runner";
+          group = "forgejo-runner";
+        };
+        grafana-secret-key = {
+          file = ../../secrets/grafana-secret-key.age;
+          mode = "0400";
+          owner = "grafana";
+          group = "grafana";
+        };
+      }
+      (lib.mkIf config.myConfig.rr.enable {
+        mullvad-wg = {
+          file = ../../secrets/mullvad-wg.age;
+          mode = "0400";
+        };
+        prowlarr-milkie-apikey = {
+          file = ../../secrets/prowlarr-milkie-apikey.age;
+          mode = "0400";
+          owner = "prowlarr";
+          group = "prowlarr";
+        };
+        prowlarr-torrentleech-password = {
+          file = ../../secrets/prowlarr-torrentleech-password.age;
+          mode = "0400";
+          owner = "prowlarr";
+          group = "prowlarr";
+        };
+        prowlarr-torrentleech-2fa = {
+          file = ../../secrets/prowlarr-torrentleech-2fa.age;
+          mode = "0400";
+          owner = "prowlarr";
+          group = "prowlarr";
+        };
+        prowlarr-seedpool-apikey = {
+          file = ../../secrets/prowlarr-seedpool-apikey.age;
+          mode = "0400";
+          owner = "prowlarr";
+          group = "prowlarr";
+        };
+      })
+    ];
   };
 }
