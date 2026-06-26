1fc6805a2bf7f8b43786d0b680d78b42741e09a5
vMohammad24 <git@vmohammad.dev>
2026-06-26 14:12:15 +0300

    format

--- modules/system/hardware.nix
+++ modules/system/hardware.nix
@@ -5,46 +5,46 @@
   ...
 }: {
   config = lib.mkIf config.myConfig.isDesktop {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
 
-    extraPackages = with pkgs; [
-      rocmPackages.clr.icd
-      rocmPackages.clr
+      extraPackages = with pkgs; [
+        rocmPackages.clr.icd
+        rocmPackages.clr
+      ];
+    };
+
+    services.xserver.videoDrivers = lib.mkDefault ["amdgpu"];
+
+    boot.initrd.kernelModules = [
+      "amdgpu"
     ];
-  };
 
-  services.xserver.videoDrivers = lib.mkDefault ["amdgpu"];
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      GDK_BACKEND = "wayland,x11";
+    };
 
-  boot.initrd.kernelModules = [
-    "amdgpu"
-  ];
+    # WlMouse (beast) 8k dongle
+    services.udev.extraRules = ''
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a868", MODE="0666", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a868", MODE="0666", TAG+="uaccess"
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a869", MODE="0666", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a869", MODE="0666", TAG+="uaccess"
+    '';
 
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    GDK_BACKEND = "wayland,x11";
-  };
+    boot.supportedFilesystems = ["ntfs3"];
 
-  # WlMouse (beast) 8k dongle
-  services.udev.extraRules = ''
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a868", MODE="0666", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a868", MODE="0666", TAG+="uaccess"
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a869", MODE="0666", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="36a7", ATTRS{idProduct}=="a869", MODE="0666", TAG+="uaccess"
-  '';
-
-  boot.supportedFilesystems = ["ntfs3"];
-
-  fileSystems."/mnt/SSSD" = {
-    device = "/dev/disk/by-uuid/8b556154-5ed1-478c-94e0-38567f794758";
-    fsType = "xfs";
-    options = [
-      "defaults"
-      "noatime"
-      "nofail"
-      "x-gvfs-show"
-    ];
-  };
+    fileSystems."/mnt/SSSD" = {
+      device = "/dev/disk/by-uuid/8b556154-5ed1-478c-94e0-38567f794758";
+      fsType = "xfs";
+      options = [
+        "defaults"
+        "noatime"
+        "nofail"
+        "x-gvfs-show"
+      ];
+    };
   };
 }
