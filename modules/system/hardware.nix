3e16cede0867299329004572118737a22aa2e9f4
vMohammad <git@vmohammad.dev>
2026-07-08 12:23:37 +0300

    networking stuff

--- modules/system/hardware.nix
+++ modules/system/hardware.nix
@@ -55,5 +55,19 @@
         "x-gvfs-show"
       ];
     };
+
+    # network stuff
+    boot.kernel.sysctl = {
+      "net.core.default_qdisc" = "fq";
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.core.rmem_max" = 16777216;
+      "net.core.wmem_max" = 16777216;
+      "net.ipv4.tcp_rmem" = "4096 87380 16777216";
+      "net.ipv4.tcp_wmem" = "4096 65536 16777216";
+      "net.core.netdev_max_backlog" = 16384;
+      "net.ipv4.tcp_mtu_probing" = 1;
+      "net.ipv4.tcp_fastopen" = 3;
+      "net.ipv4.tcp_slow_start_after_idle" = 0;
+    };
   };
 }
