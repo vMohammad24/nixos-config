0e99893cfd89caadcedc37caaa86372e0af2a323
vMohammad <git@vmohammad.dev>
2026-09-11 22:40:01 +0300

    update flake lock

--- flake.nix
+++ flake.nix
@@ -79,7 +79,7 @@
       inputs.systems.follows = "systems";
     };
     nixarr = {
-      url = "github:nix-media-server/nixarr";
+      url = "github:nix-media-server/nixarr/pull/181/merge";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     rffmpeg = {
