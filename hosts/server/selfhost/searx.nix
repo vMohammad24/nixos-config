dc05e1ee0640d4aa3a646eb6c31cd82a2bde3b84
vMohammad <git@vmohammad.dev>
2026-09-20 00:04:18 +0300

    searx: configure engines

--- hosts/server/selfhost/searx.nix
+++ hosts/server/selfhost/searx.nix
@@ -15,6 +15,81 @@ in {
         secret_key = "$SEARX_SECRET_KEY";
         limiter = false;
       };
+      engines = [
+        {
+          name = "bing";
+          disabled = false;
+        }
+        {
+          name = "yandex";
+          disabled = false;
+        }
+        {
+          name = "mwmbl";
+          disabled = false;
+        }
+        {
+          name = "wiby";
+          disabled = false;
+        }
+
+        {
+          name = "wikidata";
+          disabled = true;
+        }
+        {
+          name = "qwant";
+          disabled = true;
+        }
+        {
+          name = "yahoo";
+          disabled = true;
+        }
+        {
+          name = "reuters";
+          disabled = true;
+        }
+        {
+          name = "mojeek";
+          disabled = true;
+        }
+        {
+          name = "crowdview";
+          disabled = true;
+        }
+        {
+          name = "pypi";
+          disabled = true;
+        }
+        {
+          name = "arch linux wiki";
+          disabled = true;
+        }
+        {
+          name = "kickass";
+          disabled = true;
+        }
+        {
+          name = "vimeo";
+          disabled = true;
+        }
+        {
+          name = "dailymotion";
+          disabled = true;
+        }
+        {
+          name = "bandcamp";
+          disabled = true;
+        }
+        {
+          name = "solidtorrents";
+          disabled = true;
+        }
+        {
+          name = "wttr.in";
+          disabled = true;
+        }
+      ];
     };
   };
 }
